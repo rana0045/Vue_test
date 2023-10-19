472fbd4c4ed809de2dae5614da591eff651149da
uzairusman1425 <uzairusman1425@gmail.com>
2023-10-20 00:29:08 +0500

    responsiv

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,18 +1,18 @@
 <template>
-    <form @submit.prevent="login" class="w-full max-w-md mx-auto mt-10">
-      <div class="mb-4">
-        <label for="username" class="block text-gray-700 text-sm font-semibold mb-2">Username:</label>
-        <input v-model="username" id="username" type="text" class="w-full px-3 py-2 border rounded-lg" required />
-      </div>
-      <div class="mb-6">
-        <label for="password" class="block text-gray-700 text-sm font-semibold mb-2">Password:</label>
-        <input v-model="password" id="password" type="password" class="w-full px-3 py-2 border rounded-lg" required />
-      </div>
-      <div class="text-center">
-        <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">Login</button>
-      </div>
-    </form>
-  </template>
+  <form @submit.prevent="login" class="w-full max-w-md mx-auto mt-10 p-4 sm:p-6 md:p-8 lg:p-10">
+    <div class="mb-4">
+      <label for="username" class="block text-gray-700 text-base sm:text-lg md:text-xl lg:text-2xl font-semibold mb-2">Username:</label>
+      <input v-model="username" id="username" type="text" class="w-full px-3 py-2 border rounded-lg" required />
+    </div>
+    <div class="mb-6">
+      <label for="password" class="block text-gray-700 text-base sm:text-lg md:text-xl lg:text-2xl font-semibold mb-2">Password:</label>
+      <input v-model="password" id="password" type="password" class="w-full px-3 py-2 border rounded-lg" required />
+    </div>
+    <div class="text-center">
+      <button type="submit" class="px-4 py-2 sm:px-6 sm:py-3 md:px-8 md:py-4 lg:px-10 lg:py-5 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 text-base sm:text-lg md:text-xl lg:text-2xl">Login</button>
+    </div>
+  </form>
+</template>
   
   <script>
   import { login } from "@/mock-auth"; // Adjust the path as needed
